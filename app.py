4b75492f6f7b43a9e57559ae21a557543842317e
idrisschebak <idrisschebak@me.com>
2023-03-03 09:49:48 +0100

    update code

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
-import sqlfluff
-import sqlparse
 import streamlit as st
+import sqlparse
 import black
 
-def format_sql(sql_code):
-    try:
-        # Use sqlfluff to format the SQL code
-        formatted_code = sqlfluff.format(sql_code, encoding='utf-8')
-    except ImportError:
-        # If sqlfluff is not available, use sqlparse as a fallback
-        formatted_code = sqlparse.format(sql_code, reindent=True, keyword_case='upper')
-    return formatted_code
+st.title("Code Formatter")
 
-def format_python(py_code):
-    return black.format_str(py_code, mode=black.FileMode())
+language = st.selectbox("Select a language", ("SQL", "Python"))
 
-def main():
-    st.title("Code Formatter")
-    code_type = st.sidebar.radio("Select Code Type", ["SQL", "Python"])
+code_input = st.text_area("Enter code here", height=400)
 
-    if code_type == "SQL":
-        st.subheader("SQL Formatter")
-        sql_code = st.text_area("Enter SQL Code Here", height=300)
-        if st.button("Format SQL"):
-            formatted_code = format_sql(sql_code)
-            st.text_area("Formatted SQL Code", value=formatted_code, height=300)
-
+if st.button("Format"):
+    if language == "SQL":
+        formatted_code = sqlparse.format(code_input, reindent=True)
+    elif language == "Python":
+        formatted_code = black.format_str(code_input, mode=black.FileMode())
     else:
-        st.subheader("Python Formatter")
-        py_code = st.text_area("Enter Python Code Here", height=300)
-        if st.button("Format Python"):
-            formatted_code = format_python(py_code)
-            st.text_area("Formatted Python Code", value=formatted_code, height=300)
+        formatted_code = ""
 
-if __name__ == "__main__":
-    main()
+    st.text_area("Formatted code", value=formatted_code, height=400)
